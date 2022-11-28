392c40aaa27d01d55c0a1194974c2e74749f1fe9
Florian Wagner <fl.wagner@uni-tuebingen.de>
2022-11-28 13:57:24 +0100

    Add MANIFEST.in to include cffi directory in dist package.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description=" Python CFFI bindings for PDFlib.",
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
-    version="9.3.1.1",
+    version="9.3.1.2",
     author="Florian Wagner",
     author_email="florian@wagner-flo.net",
     python_requires=">=3.8",
